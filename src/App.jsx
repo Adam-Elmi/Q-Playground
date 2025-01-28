b7c6975fe49d36504c4f5d7600c49f2f0aa9f10b
Adam-Elmi <Adamcade123@gmail.com>
2025-01-28 21:10:24 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import Layout from './Layout';
+import './App.css';
+import { ValueContext } from './ValueContext';
+import { useState } from 'react';
 
 function App() {
-
+  const [value1, setValue1] = useState(() => {
+    const savedData = localStorage.getItem("jsonData");
+    return savedData ? savedData : "";
+  });
+  const [value2, setValue2] = useState(() => {
+    const savedData = localStorage.getItem("cssData");
+    return savedData ? savedData : "";
+  });
   return (
-    <h1>Hello from Adam</h1>
+    <ValueContext.Provider value={{ value1, setValue1, value2, setValue2 }}>
+      <Layout/>
+    </ValueContext.Provider>
   )
 }
 
