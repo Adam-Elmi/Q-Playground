a28a88b3d640fd899cf6f8a073d9f68c9ede22e9
Adam-Elmi <Adamcade123@gmail.com>
2025-01-30 17:40:38 +0300

    Added Languages.jsx

---
+++ src/sections/Languages.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function Languages({ languages, defaultValue }) {
+  const [values, setValues] = useState(defaultValue);
+  useEffect(() => {
+    setValues(defaultValue);
+  }, [defaultValue]);
+  console.log(values);
+  console.log("Hello")
+  return (
+    <div id={languages.id}>
+      <div id="languages-title">{languages.title}</div>
+      <div id="languages-wrapper">
+        {values && Array.isArray(values)
+          ? values.map((language, i) => (
+              <div key={i}>
+                <div id={"language-" + (i + 1) + "wrapper"}>
+                  <p id={"language-" + (i + 1)}>{language}</p>
+                </div>
+              </div>
+            ))
+          : ""}
+      </div>
+    </div>
+  );
+}
