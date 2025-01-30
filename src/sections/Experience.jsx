3a43e8824583f0ab985a15a5d5e2734324e25094
Adam-Elmi <Adamcade123@gmail.com>
2025-01-30 15:19:19 +0300

    Added Experience.jsx

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function Experience({ experience, defaultValue }) {
+  const [values, setValues] = useState(defaultValue);
+
+  useEffect(() => {
+    setValues(defaultValue);
+  }, [defaultValue]);
+  console.log(values);
+  return (
+    <div id={experience.id}>
+      <div id="experience-title">{experience.title}</div>
+      <div id="experience-wrapper">
+        {values && Array.isArray(values)
+          ? values.map((v, i) => (
+              <div key={i}>
+                <div id={"role-" + (i + 1) + "wrapper"}>
+                  <p id={"role-" + (i + 1)}>{v.role}</p>
+                </div>
+                <div id={"company-" + (i + 1) + "wrapper"}>
+                  <p id={"company-" + (i + 1)}>{v.company}</p>
+                </div>
+                <div id={"duration-" + (i + 1) + "wrapper"}>
+                  <p id={"duration-" + (i + 1)}>{v.duration}</p>
+                </div>
+                <div id={"description-" + (i + 1) + "wrapper"}>
+                  <p id={"description-" + (i + 1)}>{v.description}</p>
+                </div>
+              </div>
+            ))
+          : ""}
+      </div>
+    </div>
+  );
+}
